f06d387051d65b620693610965637fef1521aa75
Iprogramprograms <121345725+Iprogramprograms@users.noreply.github.com>
2023-06-21 22:00:29 +0500

    Create boolean.js

---
+++ boolean.js
@@ -0,0 +1,29 @@
+class Boolean {
+  getInfo() {
+    return {
+      id: 'logic_extension',
+      name: 'Logic Extension',
+      blocks: [
+        {
+          opcode: 'logic_true',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: 'True'
+        },
+        {
+          opcode: 'logic_false',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: 'False'
+        }
+      ]
+    };
+  }
+
+  logic_true() {
+    return true;
+  }
+  logic_false() {
+    return false;
+  }
+}
+
+Scratch.extensions.register(new HelloWorld());
